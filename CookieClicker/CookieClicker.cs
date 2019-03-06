e3a36b57cbfebf3a02263b857578253223a5e538
Parker Madel <pmadel12@gmail.com>
2019-03-06 13:02:29 -0600

    Add project files.

---
+++ CookieClicker/CookieClicker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace CookieClicker
+{
+    class CookieClicker
+    {
+        private readonly String COOKIE = "//*[@id='bigCookie']";
+
+        public CookieClicker()
+        {
+            GoToURL();
+        }
+
+        void GoToURL()
+        {
+            IWebDriver Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("http://orteil.dashnet.org/cookieclicker/");
+
+            var cookie = Driver.FindElement(By.XPath(this.COOKIE));
+
+            while (true)
+            {
+                cookie.Click();
+            }
+        }
+    }
+}
